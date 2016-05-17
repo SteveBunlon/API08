5c537f88618bca822306890f6d2e790c5b040f5b
samuel <s.reymondet@gmail.com>
2016-05-17 22:51:24 +0200

    Team cards + products corrections

--- app/views/contact/team.js
+++ app/views/contact/team.js
@@ -13,6 +13,12 @@ angular.module('polarApplication.team', ["ui.router"])
             });
     })
 
-    .controller("teamCtrl",["$scope","$state", function ($scope, $state){
-
+    .controller("teamCtrl",["usersService","$scope","$state", function (usersService,$scope, $state){
+        usersService.getAll().then(function($dataObject){
+            console.log('users');
+            console.log(JSON.parse($dataObject.data))
+;            $scope.users = JSON.parse($dataObject.data);
+        }, function($dataObject){
+            console.log("no users");
+        });
     }])
