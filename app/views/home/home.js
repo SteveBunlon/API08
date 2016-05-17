202aba73f7b284da1832e2bb1aa99d8a39169078
samuel <s.reymondet@gmail.com>
2016-05-17 23:36:51 +0200

    Footer + home update

--- app/views/home/home.js
+++ app/views/home/home.js
@@ -10,6 +10,26 @@ angular.module('polarApplication.home', ["ui.router"])
     });
 })
 
+.directive("contenteditable","$sanitize", function($sanitize) {
+    return {
+        restrict: "A",
+        require: "ngModel",
+        link: function(scope, element, attrs, ngModel) {
+
+            function read() {
+                ngModel.$setViewValue(element.html());
+            }
+
+            ngModel.$render = function() {
+                element.html($sanitize(ngModel.$viewValue || ""));
+            };
+
+            element.bind("blur keyup change", function() {
+                scope.$apply(read);
+            });
+        }
+    };
+})
 .controller("homeCtrl",["$scope","$state", function ($scope, $state){    
     
 }])
