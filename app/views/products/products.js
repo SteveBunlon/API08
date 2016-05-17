3497edafaca7c501bb031157d5cae105da5ae258
samuel <s.reymondet@gmail.com>
2016-05-17 18:31:36 +0200

    images update, product corrections

--- app/views/products/products.js
+++ app/views/products/products.js
@@ -14,6 +14,7 @@ angular.module('polarApplication.products', ["ui.router"])
 })
 
 .controller("productsCtrl",["productService","$scope","$state", "$stateParams","$http","$timeout", function (productService,$scope, $state, $stateParams, $http, $timeout){
+    $scope.orig = angular.copy($scope.data);
 
     productService.getProducts().then(function($dataObject){
         $scope.products = JSON.parse($dataObject.data);
@@ -30,7 +31,6 @@ angular.module('polarApplication.products', ["ui.router"])
         })
     };
 
-
     $scope.createProduct = function(){
         if($scope.name || $scope.category || $scope.price) {
             productService.createProduct($scope.name,$scope.category,$scope.price,$scope.onSale).then(function(){
@@ -40,18 +40,36 @@ angular.module('polarApplication.products', ["ui.router"])
                 console.error(err);
             });
         }
-    }
+    };
+
+    $scope.reset = function(){
+        $scope.name = '';
+        $scope.price = null;
+        $scope.onSale = true;
+    };
 
     $scope.saveProduct = function(){
         productService.saveProduct($scope.name,$scope.price,$scope.onSale).then(function(){
             console.log("saved");
+            $scope.name = '';
+            $scope.price = null;
+            $scope.onSale = true;
             $state.reload();
         },function(err){
             console.error(err);
         })
-    }
+    };
+
+    $scope.toast = function(id){
+        console.log("test");
+        Materialize.fadeInImage('#'+id);
+    };
 
     $timeout(function() {
+        $('.modal-trigger').leanModal({
+                ready: function() {  } // Callback for Modal open
+            }
+        );
         Materialize.updateTextFields();
         angular.element(document).ready(function () {
             $('ul.tabs').tabs('select_tab', $stateParams.productsCategory);
